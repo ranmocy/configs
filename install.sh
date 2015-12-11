188dfab24e95f2989cdb0a631949f194707f4319
Ranmocy Sheng <ranmocy@gmail.com>
2015-12-11 14:11:12 -0800

    improve install script

--- install.sh
+++ install.sh
@@ -34,84 +34,76 @@ function link() {
   echo "Linking $source to $target..."
   ln -sfn "$source" "$target"
 }
-function select_platform() {
-  local result=$1 # default
-  if [[ $PLATFORM == 'Linux' ]]; then
-    result=$1
-  elif [[ $PLATFORM == 'Darwin' ]]; then
-    result=$2
-  fi
-  echo $result
+function success() {
+  echo "${green}$1${reset}"
 }
-function confirm () {
-  # call with a prompt string or use a default
-  read -r -p "${1:-Are you sure?}[y/n/all] " response
-  case $response in
-    [yY][eE][sS]|[yY])
-      true
-      ;;
-    all)
-      export do_all=true
-      true
-      ;;
-    *)
-      $do_all || false
-      ;;
-  esac
+function finish() {
+  success "Finish setup $1."
 }
 
 
-# Init
 git submodule update --init --recursive
+link "$CONFIGS/bin" "$HOME/bin"
+finish "Init"
 
-# Git
 link "$CONFIGS/gitconfig" "$HOME/.gitconfig"
 link "$CONFIGS/gitexclude" "$HOME/.gitexclude"
-echo "Finish setup Git."
+finish "Git"
 
-# zshrc
+link "$CONFIGS/bashrc" "$HOME/.bashrc"
+finish "Bash"
+
 link "$CONFIGS/zshrc.d" "$HOME/.zshrc.d"
 link "$HOME/.zshrc.d/zshrc.zsh" "$HOME/.zshrc"
-echo "Finish setup zsh."
+finish "zsh"
 
-# fish
 link "$CONFIGS/fish" "$HOME/.config/fish"
-echo "Finish setup fish."
+finish "fish"
 
-# SublimeText
-link \
-"$CONFIGS/sublime_text_3_config" \
-"$(select_platform \
-   "$HOME/.config/sublime-text-3/Packages/User" \
-   "$HOME/Library/Application Support/Sublime Text 3/Packages/User")"
-echo "Finish setup Sublime Text."
+link "$CONFIGS/emacs.d" "$HOME/.emacs.d"
+finish "Emacs"
 
-# Ruby
-link "$$CONFIGS/gemrc" "$HOME/.gemrc"
-link "$$CONFIGS/irbrc" "$HOME/.irbrc"
+link "$CONFIGS/gemrc" "$HOME/.gemrc"
+link "$CONFIGS/irbrc" "$HOME/.irbrc"
+finish "Ruby"
+
+link "$CONFIGS/ackrc"      "$HOME/.ackrc"
+finish "Ack"
+
+link "$CONFIGS/htoprc"     "$HOME/.htoprc"
+finish "htop"
+
+link "$CONFIGS/screenrc"   "$HOME/.screenrc"
+finish "Screen"
+
+link "$CONFIGS/tmux.conf"  "$HOME/.tmux.conf"
+finish "Tmux"
+
+link "$CONFIGS/vimrc"      "$HOME/.vimrc"
+finish "VIM"
 
 # Linux
 if [[ $PLATFORM == 'Linux' ]]; then
-    # Awesome 3
+    link "$CONFIGS/sublime_text_3_config" "$HOME/.config/sublime-text-3/Packages/User"
+    finish "Sublime Text"
+
     link "$CONFIGS/awesome_config/" "$HOME/.config/awesome/rc.lua"
+    finish "Awesome 3"
+
+    # link "$CONFIGS/Xdefaults" "$HOME/.Xdefaults"
+    # link "$CONFIGS/xinitrc" "$HOME/.xinitrc"
+    # link "$CONFIGS/xmodmap" "$HOME/.xmodmap"
+    # finish "X"
 fi
 
 # Mac
 if [[ $PLATFORM == 'Darwin' ]]; then
-    link "$CONFIGS/BetterTouchTool/" "$HOME/Library/Application Support/"
-    link "$CONFIGS/Karabiner/" "$HOME/Library/Application Support/"
-fi
+    link "$CONFIGS/sublime_text_3_config" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+    finish "Sublime Text"
 
-# Misc
-link "$CONFIGS/bin"        "$HOME/bin"
-link "$CONFIGS/ackrc"      "$HOME/.ackrc"
-link "$CONFIGS/bashrc"     "$HOME/.bashrc"
-link "$CONFIGS/emacs.d"    "$HOME/.emacs.d"
-link "$CONFIGS/gemrc"      "$HOME/.gemrc"
-link "$CONFIGS/gitconfig"  "$HOME/.gitconfig"
-link "$CONFIGS/gitexclude" "$HOME/.gitexclude"
-link "$CONFIGS/htoprc"     "$HOME/.htoprc"
-link "$CONFIGS/irbrc"      "$HOME/.irbrc"
-link "$CONFIGS/screenrc"   "$HOME/.screenrc"
-link "$CONFIGS/tmux.conf"  "$HOME/.tmux.conf"
-link "$CONFIGS/vimrc"      "$HOME/.vimrc"
+    link "$CONFIGS/BetterTouchTool/" "$HOME/Library/Application Support/"
+    finish "BetterTouchTool"
+
+    link "$CONFIGS/Karabiner/" "$HOME/Library/Application Support/"
+    finish "Karabiner"
+fi
