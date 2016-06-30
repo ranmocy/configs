5ca11c59a2208498ddb99eeb9d5c6cefb3592266
Ranmocy Sheng <ranmocy@gmail.com>
2016-06-30 08:24:06 -0700

    update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+set -e
+
 CONFIGS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $CONFIGS
+
 #git pull --recurse-submodules
 git submodule foreach --recursive 'git checkout master && git pull origin master'
+
+if [[ -d zshrc.d ]]; then
+    echo "Updating zshrc deps..."
+    cd zshrc.d
+    for d in 'oh-my-zsh' 'z' 'zsh-syntax-highlighting'; do
+        cd $d
+        git checkout master && git pull origin master
+        cd ..
+    done
+fi
