efbbbceb6c04333ac42440ae60c330067f7e4e7b
Ranmocy Sheng <ranmocy@gmail.com>
2016-10-13 16:30:51 -0700

    fix zshrc updating

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-CONFIGS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+CONFIGS="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd $CONFIGS
 
 # update configs
@@ -12,13 +18,15 @@ git pull
 git submodule foreach --recursive 'git checkout master && git pull origin master'
 
 # update zshrc
-ZSHRCD=$CONFIGS"zshrc.d"
-if [[ -d $ZSHRCD ]]; then
+echo "Checking zshrc.d..."
+if [[ -d zshrc.d ]]; then
     echo "Updating zshrc deps..."
-    cd $ZSHRCD
+    cd zshrc.d
     for d in 'oh-my-zsh' 'z' 'zsh-syntax-highlighting'; do
         cd $d
         git checkout master && git pull origin master
         cd ..
     done
+else
+    echo "zshrc.d not exist!"
 fi
