2656fa18ca88b2ca43db94072a4b26073592caf0
Ranmocy Sheng <ranmocy@gmail.com>
2018-10-21 19:04:13 -0700

    add install_packages script

---
+++ install_packages.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install iterm2 sublime-text alfred karabiner-elements bettertouchtool android-studio webstorm dropbox google-backup-and-sync
