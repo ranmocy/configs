aa5cde15ff333bf29d267fd62c7d7efef1b5b003
Ranmocy <ranmocy@gmail.com>
2026-03-26 17:58:04 +0000

    Update

--- install.sh
+++ install.sh
@@ -192,6 +192,29 @@ if [[ $PLATFORM == 'Linux' ]]; then
     link "$CONFIGS/xprofile" "$HOME/.xprofile"
     # link "$CONFIGS/xmodmap" "$HOME/.xmodmap"
     finish "X"
+
+    # eza
+    sudo mkdir -p /etc/apt/keyrings
+    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc
+    //| sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de
+    stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+    sudo chmod 644 /etc/apt/keyrings/gierens.gpg
+    /etc/apt/sources.list.d/gierens.list
+    sudo apt update
+    sudo apt install -y eza
+
+    # zoxide
+    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+    # delta
+    wget https://github.com/dandavison/delta/releases/download/0.19.1/git-delta-musl_0.19.1_amd64.deb
+    sudo apt install ./git-delta-musl_0.19.1_amd64.deb
+
+    # bat
+    sudo apt install bat
+    mkdir -p ~/.local/bin
+    ln -s /usr/bin/batcat ~/.local/bin/bat
 fi
 
 # Mac
