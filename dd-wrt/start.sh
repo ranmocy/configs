b73a99b55a90679e75fb5ee31bea820864f2ee64
Ranmocy <ranmocy@gmail.com>
2026-06-11 21:02:47 +0000

    Cleanup

--- dd-wrt/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mount --bind /opt/home /mnt
-mount --bind /opt/home /tmp/root
--- install.sh
+++ install.sh
@@ -72,127 +72,37 @@ link "$CONFIGS/gitconfig" "$HOME/.gitconfig"
 link "$CONFIGS/gitexclude" "$HOME/.gitexclude"
 finish "Git"
 
-#link "$CONFIGS/hgrc" "$HOME/.hgrc"
-#finish "Mercurial"
+link "$CONFIGS/bashrc" "$HOME/.bashrc"
+finish "Bash"
 
-#link "$CONFIGS/bashrc" "$HOME/.bashrc"
-#finish "Bash"
-
-#link "$CONFIGS/zshrc.d" "$HOME/.zshrc.d"
-#link "$HOME/.zshrc.d/zshrc.zsh" "$HOME/.zshrc"
-#finish "zsh"
+link "$CONFIGS/zshrc.d" "$HOME/.zshrc.d"
+link "$HOME/.zshrc.d/zshrc.zsh" "$HOME/.zshrc"
+finish "zsh"
 
 link "$CONFIGS/fish_config" "$HOME/.config/fish"
 finish "fish"
 
-#link "$CONFIGS/emacs_config" "$HOME/.emacs.d"
-#finish "Emacs"
+link "$CONFIGS/htoprc" "$HOME/.htoprc"
+finish "htop"
 
-#link "$CONFIGS/gemrc" "$HOME/.gemrc"
-#link "$CONFIGS/irbrc" "$HOME/.irbrc"
-#finish "Ruby"
-
-#link "$CONFIGS/pythonstartup" "$HOME/.pythonstartup"
-#finish "Python"
-
-#link "$CONFIGS/ackrc" "$HOME/.ackrc"
-#finish "Ack"
-
-#link "$CONFIGS/htoprc" "$HOME/.htoprc"
-#finish "htop"
-
-#link "$CONFIGS/screenrc" "$HOME/.screenrc"
-#finish "Screen"
-
-#link "$CONFIGS/tmux.conf" "$HOME/.tmux.conf"
-#finish "Tmux"
+link "$CONFIGS/tmux.conf" "$HOME/.tmux.conf"
+finish "Tmux"
 
 link "$CONFIGS/vimrc" "$HOME/.vimrc"
 finish "VIM"
 
-#sublime_root="$([[ $PLATFORM == 'Linux' ]] && echo "$XDG_CONFIG_HOME/sublime-text-3" || echo "$HOME/Library/Application Support/Sublime Text 3")"
-#echo "Detected Sublime Text root:$sublime_root"
-#link "$CONFIGS/sublime_text_3_config" "$sublime_root/Packages/User"
-#finish "Sublime Text"
+code_root="$([[ $PLATFORM == 'Linux' ]] && echo "$XDG_CONFIG_HOME/code" || echo "$HOME/Library/Application Support/Code")"
+echo "Detected VS Code root:$code_root"
+link "$CONFIGS/Code/keybindings.json" "$code_root/User/keybindings.json"
+link "$CONFIGS/Code/settings.json" "$code_root/User/settings.json"
+link "$CONFIGS/Code/tasks.json" "$code_root/User/tasks.json"
+link "$CONFIGS/Code/snippets" "$code_root/User/snippets"
+finish "Code"
 
-#code_root="$([[ $PLATFORM == 'Linux' ]] && echo "$XDG_CONFIG_HOME/code" || echo "$HOME/Library/Application Support/Code")"
-#echo "Detected VS Code root:$code_root"
-#link "$CONFIGS/Code/keybindings.json" "$code_root/User/keybindings.json"
-#link "$CONFIGS/Code/settings.json" "$code_root/User/settings.json"
-#link "$CONFIGS/Code/tasks.json" "$code_root/User/tasks.json"
-#link "$CONFIGS/Code/snippets" "$code_root/User/snippets"
-#finish "Code"
-
-jetbrain_root="$([[ $PLATFORM == 'Linux' ]] && echo "$XDG_CONFIG_HOME/JetBrains" || echo "$HOME/Library/Application Support/JetBrains")"
-echo "Detected Jetbrain root:$jetbrain_root"
-webstorm_target=`find "$jetbrain_root" -name 'WebStorm*' | tail -1`
-if [[ !  -z  $webstorm_target  ]]; then
-  if [[ -d "$webstorm_target/jba_config" ]]; then
-    webstorm_target="$webstorm_target/jba_config"
-  fi
-  echo "Detected WebStorm target:$webstorm_target"
-  link "$CONFIGS/Intellij/config/colors" "$webstorm_target/colors"
-  if [[ $PLATFORM == 'Linux' ]]; then
-    link "$CONFIGS/Intellij/config/keymaps" "$webstorm_target/linux.keymaps"
-  else
-    link "$CONFIGS/Intellij/config/keymaps" "$webstorm_target/keymaps"
-  fi
-else
-  warn "No WebStorm found!"
-fi
-intellij_target=`find "$jetbrain_root" -name 'IdeaIC*' | tail -1`
-if [[ !  -z  $intellij_target  ]]; then
-  if [[ -d "$intellij_target/jba_config" ]]; then
-    intellij_target="$intellij_target/jba_config"
-  fi
-  echo "Detected IdeaIC target:$intellij_target"
-  link "$CONFIGS/Intellij/config/colors" "$intellij_target/colors"
-  link "$CONFIGS/Intellij/config/keymaps" "$intellij_target/keymaps"
-else
-  warn "No IdeaIC found!"
-fi
-intellij_target=`find "$jetbrain_root" -name 'IntelliJIdea*' | tail -1`
-if [[ !  -z  $intellij_target  ]]; then
-  if [[ -d "$intellij_target/jba_config" ]]; then
-    intellij_target="$intellij_target/jba_config"
-  fi
-  echo "Detected IntelliJIdea target:$intellij_target"
-  link "$CONFIGS/Intellij/config/colors" "$intellij_target/colors"
-  link "$CONFIGS/Intellij/config/keymaps" "$intellij_target/keymaps"
-else
-  warn "No IntelliJIdea found!"
-fi
-studio_root="$([[ $PLATFORM == 'Linux' ]] && echo "$XDG_CONFIG_HOME/Google" || echo "$HOME/Library/Application Support/Google")"
-studio_target=`find "${studio_root}" -name 'AndroidStudio*' | tail -1`
-if [[ !  -z  $studio_target  ]]; then
-  echo "Detected AndroidStudio target:$studio_target"
-  link "$CONFIGS/Intellij/config/colors" "$studio_target/colors"
-  link "$CONFIGS/Intellij/config/keymaps" "$studio_target/keymaps"
-else
-  warn "No AndroidStudio found!"
-fi
 finish "Preferences"
 
 # Linux
 if [[ $PLATFORM == 'Linux' ]]; then
-    # Setup mouse config
-    # sudo cp "$CONFIGS/usr/share/X11/50-marble-mouse.conf" /usr/share/X11/xorg.conf.d/50-marble-mouse.conf
-    # sudo cp "$CONFIGS/usr/share/X11/50-logitech-mx-ergo.conf" /usr/share/X11/xorg.conf.d/50-logitech-mx-ergo.conf
-
-    # i3
-    link "$CONFIGS/i3" "$HOME/.i3"
-    link "$CONFIGS/i3" "$HOME/.config/i3"
-    link "$HOME/.i3/i3status.conf" "$HOME/.i3status.conf"
-
-    link "$CONFIGS/awesome_config" "$XDG_CONFIG_HOME/awesome"
-    finish "Awesome 3"
-
-    link "$CONFIGS/Xdefaults" "$HOME/.Xdefaults"
-    link "$CONFIGS/xinitrc" "$HOME/.xinitrc"
-    link "$CONFIGS/xprofile" "$HOME/.xprofile"
-    # link "$CONFIGS/xmodmap" "$HOME/.xmodmap"
-    finish "X"
-
     # eza
     sudo mkdir -p /etc/apt/keyrings
     wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
@@ -205,13 +115,13 @@ if [[ $PLATFORM == 'Linux' ]]; then
     curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
     # delta
-    wget https://github.com/dandavison/delta/releases/download/0.19.1/git-delta-musl_0.19.1_amd64.deb
-    sudo apt install ./git-delta-musl_0.19.1_amd64.deb
+    wget https://github.com/dandavison/delta/releases/download/0.19.1/git-delta-musl_0.19.1_amd64.deb -O /tmp/git-delta-musl.deb
+    sudo apt install /tmp/git-delta-musl.deb
 
     # bat
     sudo apt install bat
     mkdir -p ~/.local/bin
-    ln -s /usr/bin/batcat ~/.local/bin/bat
+    ln -sf /usr/bin/batcat ~/.local/bin/bat
 fi
 
 # Mac
@@ -220,33 +130,21 @@ if [[ $PLATFORM == 'Darwin' ]]; then
     defaults write com.apple.Safari NSUserKeyEquivalents -dict-add 'Close Tab' '<string>@w</string></dict>'
     defaults write com.apple.universalaccess com.apple.custommenu.apps -array-add '<string>com.apple.Safari</string>'
 
-    #link_default "Library/Application Support/BetterTouchTool/bttdata2"
-    #finish "BetterTouchTool"
-
     link "$CONFIGS/karabiner" "$HOME/.config/karabiner"
     finish "Karabiner-Element"
-
-    #link "$CONFIGS/hammerspoon" "$HOME/.hammerspoon"
-    #finish "Hammerspoon"
 
     link_default "Library/Developer/Xcode/UserData/FontAndColorThemes"
     link_default "Library/Developer/Xcode/UserData/KeyBindings"
     finish "Xcode"
 
-    #link_default "Library/KeyBindings/DefaultKeyBinding.dict"
-    #finish "DefaultKeyBinding"
-
     #link_default "Library/texmf"
     #finish "texmf"
 
     # Disable ReportCrash
-    #launchctl unload -w /System/Library/LaunchAgents/com.apple.ReportCrash.plist
-    #sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.ReportCrash.Root.plist
+    launchctl unload -w /System/Library/LaunchAgents/com.apple.ReportCrash.plist
+    sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.ReportCrash.Root.plist
 
 fi
-
-# Github codespaces
-#sudo rm /.codespaces/bin/gitcredential_github.sh || true
 
 #source $CONFIGS/packages_install.sh
 #finish "Packages"
